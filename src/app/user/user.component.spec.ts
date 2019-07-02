a537fefc0399fc482fe3ad7cce3b5941dc21933b
Benoit Jaunin <jaunin.b@fr.ibm.com>
2019-07-02 17:50:17 +0200

    Fix http unit testing with the help of https://medium.com/netscape/testing-with-the-angular-httpclient-api-648203820712 https://github.com/cironunes/httpclient-testing

--- src/app/user/user.component.spec.ts
+++ src/app/user/user.component.spec.ts
@@ -1,23 +1,31 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import {RouterTestingModule} from '@angular/router/testing';
 
 import { UserComponent } from './user.component';
 import { UserService } from './user.service';
 
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+
+class FakeUSerService {
+  getUsers() {
+    return Observable.of([]);
+  }
+}
 describe('UserComponent', () => {
   let component: UserComponent;
   let fixture: ComponentFixture<UserComponent>;
 
-  beforeEach(async(() => {
-    const userServiceSpy = jasmine.createSpyObj('UserService', ['getUsers', 'deleteUser', 'createUser']);
+  beforeEach(async(() => { 
     TestBed.configureTestingModule({
       declarations: [ UserComponent ],
       imports: [
-        RouterTestingModule
+        RouterTestingModule, HttpClientTestingModule
       ],
       providers: [
-        { provide: UserService, useValue: userServiceSpy }
+        { provide: UserService, useClass: FakeUSerService }
       ]
     })
       .compileComponents();
