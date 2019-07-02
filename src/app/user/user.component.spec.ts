15d02489e2a6e212dfbcac181af633bf4fedd563
Benoit Jaunin <jaunin.b@fr.ibm.com>
2019-07-02 15:54:37 +0200

    Update UserComponent with RouterTestingModule

--- src/app/user/user.component.spec.ts
+++ src/app/user/user.component.spec.ts
@@ -1,5 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import {RouterTestingModule} from '@angular/router/testing';
+
 import { UserComponent } from './user.component';
 
 describe('UserComponent', () => {
@@ -8,7 +10,10 @@ describe('UserComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserComponent ]
+      declarations: [ UserComponent ],
+      imports: [
+        RouterTestingModule
+      ]
     })
       .compileComponents();
   }));
