4930e596cc00379b61f34f564499aec92c7c5513
Benoit Jaunin <jaunin.b@fr.ibm.com>
2019-07-02 16:20:01 +0200

    Add missing requires in TestBed

--- src/app/user/user.component.spec.ts
+++ src/app/user/user.component.spec.ts
@@ -3,16 +3,21 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import {RouterTestingModule} from '@angular/router/testing';
 
 import { UserComponent } from './user.component';
+import { UserService } from './user/user.service';
 
 describe('UserComponent', () => {
   let component: UserComponent;
   let fixture: ComponentFixture<UserComponent>;
 
   beforeEach(async(() => {
+    const userServiceSpy = jasmine.createSpyObj('UserService', ['getUsers', 'deleteUser', 'createUser']);
     TestBed.configureTestingModule({
       declarations: [ UserComponent ],
       imports: [
         RouterTestingModule
+      ],
+      providers: [
+        { provide: UserService, useValue: userServiceSpy }
       ]
     })
       .compileComponents();
